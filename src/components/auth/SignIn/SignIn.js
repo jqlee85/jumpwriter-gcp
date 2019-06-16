68cd8b427324292279a7f1023998d96fa57bdfc2
Jesse Lee <jqlee85@gmail.com>
2019-06-16 17:35:26 -0400

    remove fb and restyle login

--- src/components/auth/SignIn/SignIn.js
+++ src/components/auth/SignIn/SignIn.js
@@ -30,12 +30,6 @@ class SignIn extends Component {
     this.props.signIn(this.state, 'google')
   }
 
-  handleFacebookLogin = (e) => {
-    console.log('handleFacebookSignUp')
-    e.preventDefault()
-    this.props.signIn(this.state, 'facebook')
-  }
-
   handleEmailSignup = (e) => {
     console.log('handleSubmit')
     e.preventDefault();
@@ -46,12 +40,6 @@ class SignIn extends Component {
     e.preventDefault()
     console.log('handleGoogleSignUp')
     this.props.signUp(this.state, 'google');
-  }
-
-  handleFacebookSignup = (e) => {
-    console.log('handleFacebookSignUp')
-    e.preventDefault()    
-    this.props.signUp(this.state, 'facebook')
   }
 
   render() {
@@ -71,18 +59,30 @@ class SignIn extends Component {
             <input type="password" id="password" onChange={this.handleChange}/>
           </div>
           <div className="login-form-buttons">
-            <button className="login-button" onClick={this.handleEmailLogin}>Email Login</button>
-            <button className="google-auth-button" onClick={this.handleGoogleLogin}>Google Login</button>
-            <button className="facebook-auth-button" onClick={this.handleFacebookLogin}>Facebook Login</button>  
+            <button className="login-button" onClick={this.handleEmailLogin}>
+            <svg className="mail-icon" height="24px" version="1.1" viewBox="0 0 32 24" width="32px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1"><g fill="#fff"><path d="M5.31500722,9.27391933 C5.12106043,9.45739405 5,9.71770487 5,10.0068455 L5,21.9931545 C5,22.2828882 5.11915965,22.5440384 5.31310687,22.7277332 L5.31310687,22.7277332 L13.05,16 L5.31500722,9.27391933 L5.31500722,9.27391933 L5.31500722,9.27391933 Z M27.6868931,9.27226685 C27.8808404,9.45596162 28,9.71711185 28,10.0068455 L28,21.9931545 C28,22.2822951 27.8789396,22.542606 27.6849928,22.7260807 L19.95,16 L27.6868931,9.27226685 L27.6868931,9.27226685 Z M19.2018297,16.6505829 L26.5,23 L6.5,23 L13.7981703,16.6505829 L16.5,19 L19.2018297,16.6505829 L19.2018297,16.6505829 Z M6.00359486,8 C4.89703997,8 4,8.89451376 4,9.99406028 L4,22.0059397 C4,23.1072288 4.88976324,24 6.00359486,24 L26.9964051,24 C28.10296,24 29,23.1054862 29,22.0059397 L29,9.99406028 C29,8.8927712 28.1102368,8 26.9964051,8 L6.00359486,8 L6.00359486,8 Z M16.5,17.7000122 L26.5,9 L6.5,9 L16.5,17.7000122 L16.5,17.7000122 Z"/></g></g></svg>
+              Email Login
+            </button>
+            <button className="google-auth-button" onClick={this.handleGoogleLogin}>
+              <svg className="google-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48">
+                <defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlinkHref="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+              Google Login
+            </button>
           </div>
           <div className="error-message">
             { authError ? <p>{authError}</p> : null }
           </div>
           <h5 className="signup-form-text">Not a user? Sign up</h5>
           <div className="signup-form-buttons">
-            <button className="login-button" onClick={this.handleEmailSignup}>Email Signup</button>
-            <button className="google-auth-button" onClick={this.handleGoogleSignup}>Google Signup</button>  
-            <button className="facebook-auth-button" onClick={this.handleFacebookSignup}>Facebook Signup</button>  
+            <button className="login-button" onClick={this.handleEmailSignup}>
+              <svg className="mail-icon" height="24px" version="1.1" viewBox="0 0 32 24" width="32px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1"><g fill="#fff"><path d="M5.31500722,9.27391933 C5.12106043,9.45739405 5,9.71770487 5,10.0068455 L5,21.9931545 C5,22.2828882 5.11915965,22.5440384 5.31310687,22.7277332 L5.31310687,22.7277332 L13.05,16 L5.31500722,9.27391933 L5.31500722,9.27391933 L5.31500722,9.27391933 Z M27.6868931,9.27226685 C27.8808404,9.45596162 28,9.71711185 28,10.0068455 L28,21.9931545 C28,22.2822951 27.8789396,22.542606 27.6849928,22.7260807 L19.95,16 L27.6868931,9.27226685 L27.6868931,9.27226685 Z M19.2018297,16.6505829 L26.5,23 L6.5,23 L13.7981703,16.6505829 L16.5,19 L19.2018297,16.6505829 L19.2018297,16.6505829 Z M6.00359486,8 C4.89703997,8 4,8.89451376 4,9.99406028 L4,22.0059397 C4,23.1072288 4.88976324,24 6.00359486,24 L26.9964051,24 C28.10296,24 29,23.1054862 29,22.0059397 L29,9.99406028 C29,8.8927712 28.1102368,8 26.9964051,8 L6.00359486,8 L6.00359486,8 Z M16.5,17.7000122 L26.5,9 L6.5,9 L16.5,17.7000122 L16.5,17.7000122 Z"/></g></g></svg>
+              Email Signup
+            </button>
+            <button className="google-auth-button" onClick={this.handleGoogleSignup}>
+              <svg className="google-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48">
+                <defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlinkHref="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+                Google Signup
+            </button> 
           </div>
         </form>
       </div>
